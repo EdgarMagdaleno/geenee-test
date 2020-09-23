c15198ff9a32fdcc3dd417792214441b126fd499
Edgar Magdaleno <edgarmv97@gmail.com>
2020-09-23 09:30:57 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MainScene from './MainScene';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<MainScene />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
